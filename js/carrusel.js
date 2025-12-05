564371ec794b4317cdb4d9c3dc220e2469eb192c
sassan225 <llanosmedranolukas@gmail.com>
2025-12-05 07:11:28 -0400

    Se corrigieron las conexiones entre el html el css y el js

---
+++ js/carrusel.js
@@ -0,0 +1,56 @@
+export function initCarrusel() {
+  var track = document.querySelector(".carousel-track");
+  var slides = Array.prototype.slice.call(track.children);
+  var prevBtn = document.querySelector(".prev");
+  var nextBtn = document.querySelector(".next");
+
+  var slideWidth = slides[0].offsetWidth + 10;
+  var index = 0;
+
+  slides.forEach(function(slide) {
+    track.appendChild(slide.cloneNode(true));
+  });
+
+  var originalSlidesCount = slides.length;
+
+  function moveCarousel() {
+    track.style.transition = "transform 0.5s ease";
+    track.style.transform = "translateX(-" + (index * slideWidth) + "px)";
+
+    if (index >= originalSlidesCount) {
+      setTimeout(function() {
+        track.style.transition = "none";
+        index = 0;
+        track.style.transform = "translateX(-" + (index * slideWidth) + "px)";
+      }, 500);
+    }
+
+    if (index < 0) {
+      setTimeout(function() {
+        track.style.transition = "none";
+        index = originalSlidesCount - 1;
+        track.style.transform = "translateX(-" + (index * slideWidth) + "px)";
+      }, 500);
+    }
+  }
+
+  nextBtn.addEventListener("click", function() {
+    index++;
+    moveCarousel();
+  });
+
+  prevBtn.addEventListener("click", function() {
+    index--;
+    moveCarousel();
+  });
+
+  setInterval(function() {
+    index++;
+    moveCarousel();
+  }, 3000);
+
+  window.addEventListener("resize", function() {
+    slideWidth = slides[0].offsetWidth + 10;
+    moveCarousel();
+  });
+}
